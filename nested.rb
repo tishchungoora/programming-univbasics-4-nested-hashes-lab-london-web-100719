e79b244ba30a5c66bab2411aace9c72188943f4a
Tish Chungoora <tishal82@gmail.com>
2019-09-17 14:23:19 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -106,9 +106,9 @@ def changing_alan
         }
      }
      
-     #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
-     programmer_hash[:alan_kay][:known_for] = alans_new_info
+  #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
+  alans_new_info = "GUI"
+  programmer_hash[:alan_kay][:known_for] = alans_new_info
      
   programmer_hash
 end
@@ -129,5 +129,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+  
+  
 end
